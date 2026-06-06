cbafdc5d1697c798dc3781909ec330597d0507d0
Prajwalteggi <teggipajji@gmail.com>
2026-06-06 14:18:47 +0530

    project created

---
+++ src/test/java/com/prajwal/medisync/MedisyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prajwal.medisync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedisyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
